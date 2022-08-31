091218bbf8ab871f7de755cfc9a8d5fae7a7f7e9
Steven Torres <StevenTorres7@yahoo.com>
2022-08-31 12:57:39 -0400

    commit tree 908454000e8d8fbc315fac1bba4368407c573448

---
+++ index.js
@@ -0,0 +1,36 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+
+  function logShout(string) {
+    const String = 'HELLO';
+    console.log(String);
+    return console.log(String.toUpperCase);
+  }
+
+  function logWhisper(string) {
+    const String = 'hello';
+    console.log(String);
+    return console.log(String.toLowerCase);
+  }
+
+  function sayHiToHeadphonedRoommate (string) {
+    const a = "I can't hear you!";
+    const b = "YES INDEED!";
+    const c = "I would love to!";
+    const String = 'Let\'s have dinner together!';
+    if (string.toLowerCase(string) === string) {
+      return a;
+    }
+    else if (string.toUpperCase(string) === string) {
+      return b;
+    }
+    else if ( string) {
+      return c;
+    }
+  }
+
